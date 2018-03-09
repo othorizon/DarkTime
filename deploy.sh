bc687b66b9319650513cf549d069310d9200e49e
zhanghao <zhanghao6@kingsoft.com>
2018-03-09 16:16:03 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -x
+set -ex
+mes=$1
 echo start deploy
 summary -b -t;mv _summary.md SUMMARY.md
 summary -d
 git pull
-git acm "update"
+git status
+git acm $mes
 git push
 git status
